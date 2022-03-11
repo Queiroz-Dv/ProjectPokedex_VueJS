804c23dd30a8ffb61316958a9bacb38e19dec4c9
Queiroz <kalifor_dbel@hotmail.com>
2022-03-11 18:39:38 -0300

    Commit final

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(poke, index) in pokemons" :key="index">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import Pokemon from "./components/Pokemon.vue";
 export default {
   name: "App",
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((response) => {
+        this.pokemons = response.data.results;
+      });
+  },
+  components: {
+    Pokemon,
+  },
 };
 </script>
 
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="pokemon">
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img :src="currentImg" alt="Placeholder image" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{ number }} - {{ name }}</p>
+            <p class="subtitle is-6">{{ pokemon.type }}</p>
+          </div>
+        </div>
+        <div class="content"></div>
+        <button class="button is-medium is-fullwidth" @click="changeSprite">Sprite</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  created: function () {
+    axios.get(this.url).then((response) => {
+      this.pokemon.type = response.data.types[0].type.name;
+      this.pokemon.front = response.data.sprites.front_default;
+      this.pokemon.back = response.data.sprites.back_default;
+      this.currentImg = this.pokemon.front;
+    });
+  },
+  data() {
+    return {
+      isFront: true,
+      currentImg: "",
+      pokemon: {
+        type: "",
+        front: "",
+        back: "",
+      },
+    };
+  },
+  props: {
+    num: Number,
+    name: String,
+    url: String,
+  },
+  methods: {
+    changeSprite: function () {
+      if (this.isFront) {
+        this.isFront = false;
+        this.currentImg = this.pokemon.back;
+      } else {
+        this.isFront = true;
+        this.currentImg = this.pokemon.front;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#pokemon {
+  margin-top: 2%;
+}
+</style>
